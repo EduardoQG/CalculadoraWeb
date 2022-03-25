1c69632ce5cb1d8e5bc465d5843b8a5b98b37b03
EduardoQG <eduquiroga8@gmail.com>
2022-03-25 12:22:43 +0100

    Added rest function. Corrected some bugs: making concatenated operations, depending on the last operator pressed.

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,53 @@
+let acumulado=0;
+
+let sumCheck=false; let restCheck=false;
+
 $(document).ready(function() {
 	$(".button-number").click(function() {
 		let valueToDisplay = $("#display").val() === "0" ? $(this).html() : $("#display").val() + $(this).html();
 		// Se usan operadores ternarios. Si el valor de display es 0, se almacena lo que se ha pulsado; si no es 0, se suma a lo anterior.
+		// 'this' hace referencia al botón pulsado, y .html hace referencia a lo que hay escrito dentro del botón
 		$("#display").val(valueToDisplay);
 	});
 });
 
-function clearDisplay(){
-	$("#display").val("0");
-	acumuado=0;
-}
-
-let acumulado=0;
-
-function sum(){
-   
-	acumulado=parseInt($("#display").val())+parseInt(acumulado);
-	$("#display").val(0);
+function reset(){
 	
+	$("#display").val("0");
+	
+	acumulado=0;
+
+	sumCheck=false; restCheck=false;
 }
 
 
 function result(){
 
-	$("#display").val(parseInt(acumulado)+parseInt($("#display").val()));
-    
+	if(sumCheck){$("#display").val(parseInt(acumulado)+parseInt($("#display").val()));}
+    if(restCheck){$("#display").val(parseInt(acumulado)-parseInt($("#display").val()));}
 }
+
+function sum(){
+   
+	if(sumCheck){acumulado=parseInt($("#display").val())+parseInt(acumulado);}
+	else if(restCheck){acumulado=parseInt(-1*(($("#display").val()))+parseInt(acumulado));}
+	else {acumulado=parseInt($("#display").val())}
+
+	$("#display").val(0);
+	
+	sumCheck=true; restCheck=false;
+}
+
+function rest(){
+	
+	if(sumCheck){acumulado=parseInt($("#display").val())+parseInt(acumulado);}
+	else if(restCheck){acumulado=parseInt(-1*(($("#display").val()))+parseInt(acumulado));}
+	else{acumulado=parseInt($("#display").val())}
+	$("#display").val(0);
+
+	sumCheck=false; restCheck=true;
+
+
+}
+
+
