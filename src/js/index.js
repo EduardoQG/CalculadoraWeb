0ef0ab3ac585a46d6f1f19b58e6515610228c44b
EduardoQG <eduquiroga8@gmail.com>
2022-03-25 14:00:28 +0100

    Added division. Disabled input text writing on screen.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-let acumulado=0;
+let acumulado=0; let negativeNumberCheck=true;
 
 let sumCheck=false; let restCheck=false; let multCheck=false; let divisionCheck=false;
 
 $(document).ready(function() {
 	$(".button-number").click(function() {
+		
+		negativeNumberCheck=false;
 		let valueToDisplay = $("#display").val() === "0" ? $(this).html() : $("#display").val() + $(this).html();
-		// Se usan operadores ternarios. Si el valor de display es 0, se almacena lo que se ha pulsado; si no es 0, se suma a lo anterior.
-		// 'this' hace referencia al botón pulsado, y .html hace referencia a lo que hay escrito dentro del botón
 		$("#display").val(valueToDisplay);
 	});
 });
@@ -18,50 +18,94 @@ function reset(){
 	acumulado=0;
 
 	sumCheck=false; restCheck=false; multCheck=false; divisionCheck=false;
+
+	negativeNumberCheck=true;
 }
 
 
 function result(){
 
+
+	negativeNumberCheck=true;
+
 	if(sumCheck){$("#display").val(parseInt(acumulado)+parseInt($("#display").val()));}
     if(restCheck){$("#display").val(parseInt(acumulado)-parseInt($("#display").val()));}
 	if(multCheck){$("#display").val(parseInt(acumulado)*parseInt($("#display").val()));}
+    if(divisionCheck){$("#display").val(parseInt(acumulado)/parseInt($("#display").val()));}
 
 	sumCheck=false; restCheck=false; multCheck=false; divisionCheck=false;
 }
 
 function sum(){
    
+	
+
 	if(sumCheck){acumulado=parseInt($("#display").val())+parseInt(acumulado);}
 	else if(restCheck){acumulado=parseInt(-1*(($("#display").val()))+parseInt(acumulado));}
+	else if(multCheck){acumulado=parseInt($("#display").val())*parseInt(acumulado);}
+	else if(divisionCheck){acumulado=parseInt(acumulado)/parseInt($("#display").val());}
 	else {acumulado=parseInt($("#display").val())}
 
 	$("#display").val(0);
+
+	negativeNumberCheck=true;
 	
 	sumCheck=true; restCheck=false; multCheck=false; divisionCheck=false;
 }
 
 function rest(){
 	
-	if(sumCheck){acumulado=parseInt($("#display").val())+parseInt(acumulado);}
-	else if(restCheck){acumulado=parseInt(-1*(($("#display").val()))+parseInt(acumulado));}
-	else{acumulado=parseInt($("#display").val())}
+	if(negativeNumberCheck==true){
+
+		$("#display").val("-");
+
+	} else{
+
+		if(sumCheck){acumulado=parseInt($("#display").val())+parseInt(acumulado);}
+		else if(restCheck){acumulado=parseInt(-1*(($("#display").val()))+parseInt(acumulado));}
+		else if(multCheck){acumulado=parseInt($("#display").val())*parseInt(acumulado);}
+		else if(divisionCheck){acumulado=parseInt(acumulado)/parseInt($("#display").val());}
+		else{acumulado=parseInt($("#display").val())}
+
+	}
 	
 	$("#display").val(0);
+
+	negativeNumberCheck=true;
 
 	sumCheck=false; restCheck=true; multCheck=false; divisionCheck=false;
 }
 
 function multiplication(){
 
+	
+
 	if(sumCheck){acumulado=parseInt($("#display").val())+parseInt(acumulado);}
 	else if(restCheck){acumulado=parseInt(-1*(($("#display").val()))+parseInt(acumulado));}
 	else if(multCheck){acumulado=parseInt($("#display").val())*parseInt(acumulado);}
+	else if(divisionCheck){acumulado=parseInt(acumulado)/parseInt($("#display").val());}
 	else{acumulado=parseInt($("#display").val())}
 
 	$("#display").val(0);
 
+	negativeNumberCheck=true;
 	sumCheck=false; restCheck=false; multCheck=true; divisionCheck=false;
+}
+
+function division(){
+
+	
+
+	if(sumCheck){acumulado=parseInt($("#display").val())+parseInt(acumulado);}
+	else if(restCheck){acumulado=parseInt(-1*(($("#display").val()))+parseInt(acumulado));}
+	else if(multCheck){acumulado=parseInt($("#display").val())*parseInt(acumulado);}
+	else if(divisionCheck){acumulado=parseInt(acumulado)/parseInt($("#display").val());}
+	else{acumulado=parseInt($("#display").val())}
+
+	$("#display").val(0);
+
+	negativeNumberCheck=true;
+	sumCheck=false; restCheck=false; multCheck=false; divisionCheck=true;
 }
 
 
